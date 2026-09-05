3a50c100d7bda36694136f634cc6e095be3e242d
Mostafa Saqly <mostafasaqly1@gmail.com>
2026-09-05 15:55:25 +0300

    new commit

---
+++ Day 01/start.js
@@ -0,0 +1,3 @@
+console.log("start");
+console.log(process.platform);
+// console.log(window.innerWidth);
---
+++ Day 01/test.js
@@ -0,0 +1,9 @@
+// console.log("Starting the application...");
+
+const name = {
+  first: "John",
+  last: "Doe",
+  skills: ["JavaScript", "Node.js", "React"],
+};
+
+
