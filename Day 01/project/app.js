cbc0c09d99b580baf548e7f85cc494ed5ae3474d
Mostafa Saqly <mostafasaqly1@gmail.com>
2026-09-05 16:24:08 +0300

    add node and Dom files

--- Day 01/project/app.js
+++ Day 01/project/app.js
@@ -1,1 +1,28 @@
 console.log('New Hello, World!');
+// DOM
+const title = document.getElementById('title');
+title.style.color = 'blue';
+title.textContent = 'Hello, World! This is a new text.';
+
+const button = document.getElementById('btn');
+// button.addEventListener('click', function()
+// {
+//     title.style.color = 'green';
+//     title.textContent = 'You clicked the button!';
+//     console.log('Button clicked!');
+// });
+
+
+let count =0;
+button.addEventListener('click', function()
+{
+    count++;
+    title.textContent = `You clicked the button ${count} times!`;
+} );
+
+const resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', function()
+{  
+    count = 0;
+    title.textContent = 'Hello, World! This is a new text.';
+} );
--- Day 01/start.js
+++ Day 01/start.js
@@ -1,3 +1,27 @@
 console.log("start");
 console.log(process.platform);
 // console.log(window.innerWidth);
+
+const CourseName="NodeJS";
+const instructor ="Mostafa";
+const duration = 6;
+console.log(`Course Name: ${CourseName}, Instructor: ${instructor}, Duration: ${duration} months`);
+
+
+function greet(name)
+{
+    return `Hello ${name}`;
+}
+console.log(greet("Alice"));
+console.log(greet("mostafa"));
+
+function add(a,b)
+{
+    return a+b;
+}
+console.log(add(5, 3));
+
+
+console.log(process.version);
+console.log(process.platform);
+console.log(process.cwd());
